a1d6885217dc2c06144dbc1452480861b5c00abd
unfloat <olphazarrouk@gmail.com>
2019-02-11 00:23:17 +0100

    FOSUserBundle and UserBundle added

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new FrontBundle\FrontBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
