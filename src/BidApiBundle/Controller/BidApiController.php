2289b3903e093efef2c4528faa519745707046ab
unfloat <olphazarrouk@gmail.com>
2019-04-24 18:28:46 +0200

    api bid create and read

--- src/BidApiBundle/Controller/BidApiController.php
+++ src/BidApiBundle/Controller/BidApiController.php
@@ -2,8 +2,45 @@
 
 namespace BidApiBundle\Controller;
 
+use BidBundle\Entity\Bid;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+use Symfony\Component\Serializer\Serializer;
 
 class BidApiController extends Controller
 {
+
+    public function allBidsAction()
+    {
+        $bids = $this->getDoctrine()->getRepository(Bid::class)
+            ->findAll();
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($bids);
+        return new JsonResponse($formatted);
+    }
+
+    public function findBidAction($id)
+    {
+        $tasks = $this->getDoctrine()->getManager()
+            ->getRepository('BidBundle:Bid')
+            ->find($id);
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($tasks);
+        return new JsonResponse($formatted);
+    }
+
+    public function placeBidAction(Request $request)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $bid = new Bid();
+        $bid->setDeliveryTime($request->get('deliveryTime'));
+        $bid->setMinimalRate($request->get('hourlyRate'));
+        $em->persist($bid);
+        $em->flush();
+        $serializer = new Serializer([new ObjectNormalizer()]);
+        $formatted = $serializer->normalize($bid);
+        return new JsonResponse($formatted);
+    }
 }
